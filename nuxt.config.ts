7832b7f9df3208040d079e49eeed9ef7465fbda9
pulkit604 <guppul@gmail.com>
2023-02-22 14:42:43 +0100

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: 'src',
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['@nuxtjs/tailwindcss', 'postcss-custom-properties'],
 })
